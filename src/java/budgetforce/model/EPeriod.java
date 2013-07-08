7b418b995d6e1cc3fde1a7297ef1783d4ff7dd5a
David König <david.koenig@fh-erfurt.de>
2013-07-08 13:03:43 +0200

    added Period Enum

---
+++ src/java/budgetforce/model/EPeriod.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package budgetforce.model;
+
+/**
+ *
+ * @author kinske
+ */
+public enum EPeriod {
+    ONCE,
+    HOUR,
+    DAY,
+    MONTH,
+    QUARTER,
+    YEAR
+}
