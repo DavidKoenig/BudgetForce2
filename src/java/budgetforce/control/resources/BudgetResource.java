2088b78acc63df61db1b66377f25fa55a3e07d79
David Rochholz <david.rochholz@googlemail.com>
2013-07-09 16:00:36 +0200

    Created BudgetResource, Fixed Databasemanager, being awesome

--- src/java/budgetforce/control/resources/BudgetResource.java
+++ src/java/budgetforce/control/resources/BudgetResource.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package budgetforce.control;
+package budgetforce.control.resources;
 
 import budgetforce.model.Budget;
 import budgetforce.model.DatabaseManager;
