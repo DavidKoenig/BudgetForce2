9a2c89b92600911444f1e93d526f26d74fafa0ea
David Rochholz <david.rochholz@googlemail.com>
2013-07-10 10:39:06 +0200

    Created BudgetResource, Fixed Databasemanager, being awesome

---
+++ src/java/budgetforce/control/statistics/StatisticsResource.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package budgetforce.control.statistics;
+
+import budgetforce.control.resources.OutgoingResource;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+/**
+ * REST Web Service
+ *
+ * @author David
+ */
+@Path("/statistics")
+public class StatisticsResource {
+
+    public StatisticsResource() {
+    }
+
+    
+}
