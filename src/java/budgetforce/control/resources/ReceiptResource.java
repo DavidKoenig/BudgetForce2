43f380126712914a17ab12fb29da0a5feccf8819
Soi Fon <Soi Fon@SoiFon-PC>
2013-07-11 11:34:15 +0200

    -Implemented Receipt, and ReceiptResource Classes -Modified Index.jsp a little

--- src/java/budgetforce/control/resources/ReceiptResource.java
+++ src/java/budgetforce/control/resources/ReceiptResource.java
@@ -4,14 +4,19 @@
  */
 package budgetforce.control.resources;
 
+import budgetforce.model.DatabaseManager;
+import budgetforce.model.Receipt;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -22,24 +27,50 @@ import javax.ws.rs.Produces;
 public class ReceiptResource 
 {
 
-    @Context
-    private UriInfo context;
-
     public ReceiptResource() 
     {
     }
 
     @GET
+    @Path("{id}")
     @Produces("application/json")
-    public String getJson() 
+    public Receipt getReceiptById(@PathParam("id") Integer _Id) 
     {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        //return DatabaseManager.getDatabaseManager().getReceiptByID(_Id);
+        return new Receipt();
     }
 
-    @PUT
+    //post for creating a new entry
+    @POST
     @Consumes("application/json")
-    public void putJson(String content) 
+    @Produces("application/json")
+    public Response postReceipt(Receipt _Receipt) 
+    {     
+        //int id = DatabaseManager.getDatabaseManager().insertReceipt(_Receipt);
+        
+        return Response.status(201).entity(_Receipt).build();
+    }
+    
+    //put for updating an entry
+    @PUT
+    @Path("{id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response putReceipt(@PathParam("id") Integer _Id, Receipt _Receipt) 
     {
+        boolean successful = true;//DatabaseManager.getDatabaseManager().updateReceipt(_Receipt);
+        
+        return Response.status(201).entity(successful).build();
+    }
+    
+    @DELETE
+    @Path("{id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response deleteReceipt(@PathParam("id") Integer _Id) 
+    {
+        boolean successful = true;//DatabaseManager.getDatabaseManager().deleteReceipt(_Id);
+        
+        return Response.status(201).entity(successful).build();
     }
 }
--- src/java/budgetforce/model/Receipt.java
+++ src/java/budgetforce/model/Receipt.java
@@ -1,13 +1,77 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package budgetforce.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 
 /**
  *
  * @author Soi Fon
  */
-public class Receipt {
+@XmlRootElement
+public class Receipt 
+{
+    //---------------------------------------------------
+    // Get and set functions
+    //-------------------------------------------------- 
+    @XmlTransient
+    public int getID() {
+        return m_ID;
+    }
+
+    @XmlTransient
+    public void setID(int _ID) {
+        this.m_ID = _ID;
+    }
+
+    @XmlElement(name="path")
+    public String getPath() {
+        return m_Path;
+    }
+
+    @XmlElement(name="path")
+    public void setPath(String _Path) {
+        this.m_Path = _Path;
+    }
+
+    @XmlElement(name="file")
+    public String getFilename() {
+        return m_Filename;
+    }
+
+    @XmlElement(name="file")
+    public void setFilename(String _Filename) {
+        this.m_Filename = _Filename;
+    }
+
+    @XmlElement(name="outgoingID")
+    public int getOutgoingID() {
+        return m_OutgoingID;
+    }
+
+    @XmlElement(name="outgoingID")
+    public void setOutgoingID(int _OutgoingID) {
+        this.m_OutgoingID = _OutgoingID;
+    }
+
+    @XmlElement(name="personID")
+    public int getPersonID() {
+        return m_PersonID;
+    }
+
+    @XmlElement(name="personID")
+    public void setPersonID(int _PersonID) {
+        this.m_PersonID = _PersonID;
+    }
     
+    //---------------------------------------------------
+    // Private variables
+    //--------------------------------------------------- 
+    private int     m_ID;
+    private String  m_Path;
+    private String  m_Filename;
+    private int     m_OutgoingID;
+    private int     m_PersonID;
 }
