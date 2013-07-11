90d73ace0017199d759cc8a09cad84d578e86a74
David Rochholz <david.rochholz@googlemail.com>
2013-07-11 15:56:20 +0200

    added StatisticOutgoingCategory

---
+++ src/java/budgetforce/model/statistics/StatisticOutgoingCategory.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package budgetforce.model.statistics;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+import budgetforce.model.Outgoing;
+import budgetforce.model.Category;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author David
+ */
+@XmlRootElement
+public class StatisticOutgoingCategory {
+    
+    //---------------------------------------------------
+    // Get and set functions
+    //---------------------------------------------------
+    @XmlElement(name="categoryname")
+    public String getCategoryName() {
+        return m_Category.getName();
+    }
+    
+    @XmlElement(name="category")
+    public void setCategory(Category _Category) {
+        this.m_Category = _Category;
+    }
+    
+    @XmlElement(name="outgoings")
+    public ArrayList<Outgoing> getOutgoings() {
+        return m_Outgoings;
+    }
+    
+    @XmlElement(name="outgoings")
+    public void setOutgoings(ArrayList<Outgoing> _Outgoing) {
+        this.m_Outgoings = _Outgoing;
+    }
+    
+    @XmlElement(name="startdate")
+    public Date getStartDate() {
+        return m_Start;
+    }
+    
+    @XmlElement(name="startdate")
+    public void setStartDate(Date _Start) {
+        this.m_Start = _Start;
+    }
+    
+    @XmlElement(name="enddate")
+    public Date getEndDate() {
+        return m_End;
+    }
+    
+    @XmlElement(name="enddate")
+    public void setEndDate(Date _End) {
+        this.m_End = _End;
+    }
+    
+    @XmlElement(name="overallpercentage")
+    public Float getOverallPercentage() {
+        return m_OverallPercentage;
+    }
+    
+    @XmlElement(name="overallpercentage")
+    public void setOverallPercentage(Float _OverallPercentage) {
+        this.m_OverallPercentage = _OverallPercentage;
+    }
+    
+    @XmlElement(name="sum")
+    public Float getSum() {
+        return m_Sum;
+    }
+    
+    @XmlElement(name="sum")
+    public void setSum(Float _Sum) {
+        this.m_Sum = _Sum;
+    }
+    
+    @XmlElement(name="userid")
+    public Integer getUserId() {
+        return m_UserId;
+    }
+    
+    @XmlElement(name="userid")
+    public void setUserId(Integer _UserId) {
+        this.m_UserId = _UserId;
+    }
+    
+    //---------------------------------------------------
+    // Private variables
+    //---------------------------------------------------
+    private Category                m_Category;
+    private ArrayList<Outgoing>     m_Outgoings;
+    private Date                    m_Start;
+    private Date                    m_End;
+    private Float                   m_OverallPercentage;
+    private Float                   m_Sum;
+    private Integer                 m_UserId;
+    
+}
